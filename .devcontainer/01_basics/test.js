34a8c2ae5bc1f3663ebdc4efced1e02004ab1054
Manthan Bhardwaj <98547748+itzmanthan@users.noreply.github.com>
2024-01-05 17:00:56 +0000

    array commands

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,2 +1,6 @@
-let myarr=[1,2,3,4,5,true,"manathan"]
-myarr.push(2)
+//let myarr=[1,2,3,4,5,true,"manathan"]
+//myarr.push(2)
+//myarr.pop(3)
+const marvel_heros=["iron","thor","spiderman"]
+const dc=["superman","flash","batman"]
+marvel_heros.push(dc)
