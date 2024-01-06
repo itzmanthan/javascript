af187cb03b5f8b584f386016dd5d8fd9a9c8a167
Manthan Bhardwaj <98547748+itzmanthan@users.noreply.github.com>
2024-01-06 16:17:51 +0000

    objects

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,7 +1,17 @@
 //let myarr=[1,2,3,4,5,true,"manathan"]
 //myarr.push(2)
 //myarr.pop(3)
-const marvel_heros=["iron","thor","spiderman"]
-const dc=["superman","flash","batman"]
-marvel_heros.push(dc)
-console.log(marvel_heros);
+//const marvel_heros=["iron","thor","spiderman"]
+//const dc=["superman","flash","batman"]
+//marvel_heros.push(dc)
+//console.log(marvel_heros);
+
+//objects------///
+ 
+const mysum=Symbol("key1")
+const jsuser={
+    name:"Manthan",
+    "full name":"Manthan Bhardwaj",
+    [mysum]:"mykey1",
+    
+}
